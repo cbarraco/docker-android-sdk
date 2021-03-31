7d254134745e720642f9e9140e2810facfd6b232
Carlo Barraco <carlobarraco@gmail.com>
2021-03-31 18:33:16 -0400

    Removed logic to determine variant

--- tools/android-sdk-update.sh
+++ tools/android-sdk-update.sh
@@ -4,21 +4,6 @@ mkdir -p /opt/android-sdk-linux/bin/
 cp /opt/tools/android-env.sh /opt/android-sdk-linux/bin/
 source /opt/android-sdk-linux/bin/android-env.sh
 
-built_in_sdk=1
-
-if [ $# -ge 0 ] && [ "$1" == "lazy-dl" ]
-then
-    echo "Using Lazy Download Flavour"
-    built_in_sdk=0
-elif [ $# -ge 0 ] && [ "$1" == "built-in" ]
-then
-    echo "Using Built-In SDK Flavour"
-    built_in_sdk=1
-else
-    echo "Please use either built-in or lazy-dl as parameter"
-    exit 1
-fi
-
 cd ${ANDROID_HOME}
 echo "Set ANDROID_HOME to ${ANDROID_HOME}"
 
@@ -47,18 +32,10 @@ cp -v /opt/tools/*.sh ${ANDROID_HOME}/bin
 echo "Print sdkmanager version"
 sdkmanager --version
 
-
 echo "Installing packages"
-if [ $built_in_sdk -eq 1 ]
-then
-    while read p; do 
-      android-accept-licenses.sh "sdkmanager ${SDKMNGR_OPTS} ${p}"
-    done < /opt/tools/package-list-minimal.txt
-else
-    while read p; do
-      android-accept-licenses.sh "sdkmanager ${SDKMNGR_OPTS} ${p}"
-    done < /opt/tools/package-list.txt
-fi
+while read p; do 
+    android-accept-licenses.sh "sdkmanager ${SDKMNGR_OPTS} ${p}"
+done < /opt/tools/package-list-minimal.txt
 
 echo "Updating SDK"
 update_sdk
